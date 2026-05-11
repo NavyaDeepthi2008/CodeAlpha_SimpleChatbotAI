388550ae28091c1b5102e9b63b9409ee217916dd
NavyaDeepthi2008 <navyadeepthi2008@gmail.com>
2026-05-11 13:17:38 +0530

    Add files via upload

---
+++ ChatbotAI.py
@@ -0,0 +1,15 @@
+print("Basic ChatBot")
+print("Type Bye to exit")
+while True:
+    user_input=input("You:").lower()
+    if user_input=="hello":
+        print("ChatBot: Hi!")
+    elif user_input=="how are you":
+        print("ChatBot: I'm fine, thanks!")
+    elif user_input=="what is your name":
+        print("ChatBot: I am a Python ChatBot.")
+    elif user_input=="bye":
+        print("Goodbye!")
+        break
+    else:
+        print("ChatBot: Sorry, I don't understand your input")
